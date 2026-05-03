f4132c3af36d2b89068741a6a499cd1a6e55915a
千葉尚暉 <yasuhirochiba42@gmail.com>
2026-05-03 06:36:59 +0000

    Fix: base-href固定 + SW無効化 - 白画面完全解消

--- android/app/src/main/kotlin/com/crazystats/crazy_stats/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.crazystats.crazy_stats
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:crazy_stats/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    // Basic smoke test
-    expect(true, isTrue);
-  });
-}
