0403876fce542a3ffb406152744e5871dc356aab
千葉尚暉 <yasuhirochiba42@gmail.com>
2026-05-04 12:20:46 +0000

    Fix: GAS URL直接埋め込み - 設定画面スキップ・保存エラー解消

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2316283163"
+    serviceWorkerVersion: "2594283193"
   }
 });
