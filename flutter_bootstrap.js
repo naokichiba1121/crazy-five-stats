9b86a268e1c0b57bca091db6e5aa945e7600dc27
千葉尚暉 <yasuhirochiba42@gmail.com>
2026-05-04 10:17:22 +0000

    Fix: Hive完全除去 → GAS HTTP API移行 保存エラー根本解決

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1301277088"
+    serviceWorkerVersion: "1970152060"
   }
 });
